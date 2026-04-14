511d932ba6c189b41dbe2527f4fb9f6f293961d5
dani4god <dokeke646@gmail.com>
2026-04-14 11:25:43 -0700

    jut

--- final-project/config/oauth.js
+++ final-project/config/oauth.js
@@ -1,20 +1,26 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const { ObjectId } = require('mongodb');  // MUST HAVE THIS
 const { getDb } = require('../db/connect');
 
-// Serialize user for session
+// Serialize user - store ID as string
 passport.serializeUser((user, done) => {
-  done(null, user._id);
+  console.log('Serializing user:', user._id);
+  done(null, user._id.toString());
 });
 
-// Deserialize user from session
+// Deserialize user - MUST convert string to ObjectId
 passport.deserializeUser(async (id, done) => {
   try {
+    console.log('Deserializing ID:', id);
     const db = getDb();
-    const user = await db.collection('users').findOne({ _id: id });
+    // THIS IS THE KEY FIX - Convert string to ObjectId
+    const user = await db.collection('users').findOne({ _id: new ObjectId(id) });
+    console.log('Deserialized user:', user?.displayName);
     done(null, user);
   } catch (err) {
-    done(err, null);
+    console.error('Deserialize error:', err);
+    done(null, null);  // Return null instead of error
   }
 });
 
@@ -22,18 +28,17 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URL || 'http://localhost:3000/auth/github/callback'
+  callbackURL: process.env.GITHUB_CALLBACK_URL || 'https://cse341-final-4ubm.onrender.com/auth/github/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
+    console.log('GitHub profile:', profile.id);
     const db = getDb();
     const usersCollection = db.collection('users');
     
-    // Check if user exists
     let user = await usersCollection.findOne({ authId: profile.id.toString() });
     
     if (!user) {
-      // Create new user
       const newUser = {
         displayName: profile.username,
         firstName: profile.displayName?.split(' ')[0] || profile.username,
